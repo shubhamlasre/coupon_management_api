f530504448d28fd522b6f5f10a80cd351336c28f
Shubham Lasre <shubham.lasre@ibm.com>
2024-11-21 16:28:43 +0530

    updated test case to update bxgy coupon

--- src/test/java/com/monk/ecom/service/UpdateCouponServiceImplTest.java
+++ src/test/java/com/monk/ecom/service/UpdateCouponServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.monk.ecom.service;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,6 +15,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.monk.ecom.domain.BuyAndGetCouponMap;
 import com.monk.ecom.domain.Coupon;
 import com.monk.ecom.domain.CreationCriteria;
 import com.monk.ecom.domain.Product;
@@ -52,7 +54,7 @@ class UpdateCouponServiceImplTest {
     private couponUtility utility;
 
     @Test
-    void testUpdateCoupon_CartWise_Success() throws CouponNotFoundException {
+    void testUpdateCouponCartWiseSuccess() throws CouponNotFoundException {
         long couponId = 1L;
         Coupon mockCoupon = new Coupon("cart-wise", 10.0f, "10 off on 100");
         ProductDetails mockDetails = new ProductDetails(50.0f, 5.0f, 1L, new ArrayList<>(), new ArrayList<>(), 2);
@@ -70,10 +72,11 @@ class UpdateCouponServiceImplTest {
     }
 
     @Test
-    void testUpdateCoupon_ProductWise_Success() throws CouponNotFoundException {
+    void testUpdateCouponProductWiseSuccess() throws CouponNotFoundException {
         long couponId = 2L;
         Coupon mockCoupon = new Coupon("product-wise", 10.0f, "10 off on 100");
         ProductDetails mockDetails = new ProductDetails(50.0f, 5.0f, 1L, new ArrayList<>(), new ArrayList<>(), 2);
+
         CreationCriteria criteria = new CreationCriteria("product-wise", mockDetails);
         Product mockProduct = new Product(1l, 100f, 5, 0);
 
@@ -91,7 +94,33 @@ class UpdateCouponServiceImplTest {
     }
 
     @Test
-    void testUpdateCoupon_NotFound() {
+    void testUpdateCouponBxGySuccess() throws CouponNotFoundException {
+        long couponId = 2L;
+        Coupon mockCoupon = new Coupon("Bxgy", 10.0f, "Buy 1 of P1 and get 1 of P2");
+        ProductDetails mockDetails = new ProductDetails(50.0f, 5.0f, 1L, new ArrayList<>(), new ArrayList<>(), 2);
+        Product buyProduct = new Product(1l, 200f, 5, 0);
+        Product getProduct = new Product(2l, 300f, 5, 0);
+
+        mockDetails.setBuyProducts(List.of(buyProduct));
+        mockDetails.setGetProducts(List.of(getProduct));
+        CreationCriteria criteria = new CreationCriteria("Bxgy", mockDetails);
+        Product mockProduct = new Product(2l, 300f, 5, 0);
+
+        when(couponRepository.findById(couponId)).thenReturn(Optional.of(mockCoupon));
+        when(productRepo.findById(getProduct.getProductId())).thenReturn(Optional.of(mockProduct));
+        when(utility.getBxGyDiscountDescription(mockDetails)).thenReturn("Updated Product Description");
+
+        updateCouponService.updateCoupon(couponId, criteria);
+        BuyAndGetCouponMap mockBuyAndGetCouponMap = new BuyAndGetCouponMap(couponId, buyProduct.getProductId(), buyProduct.getQuantity(), getProduct.getProductId(), getProduct.getQuantity(), 0.0f, 2);
+        List<BuyAndGetCouponMap> mockAndGetCouponMapList = new ArrayList<>();
+        mockAndGetCouponMapList.add(mockBuyAndGetCouponMap);
+        verify(couponRepository, times(1))
+                .updateCouponDiscountAndDescription(couponId, mockDetails.getDiscount(), "Updated Product Description");
+
+    }
+
+    @Test
+    void testUpdateCouponNotFound() {
         long couponId = 3L;
         when(couponRepository.findById(couponId)).thenReturn(Optional.empty());
 
@@ -103,7 +132,7 @@ class UpdateCouponServiceImplTest {
     }
 
     @Test
-    void testDeleteCoupon_Success() throws CouponNotFoundException {
+    void testDeleteCouponSuccess() throws CouponNotFoundException {
         long couponId = 4L;
         Coupon mockCoupon = new Coupon("cart-wise", 10.0f, "10 off on 100");
 
@@ -116,7 +145,7 @@ class UpdateCouponServiceImplTest {
     }
 
     @Test
-    void testDeleteCoupon_NotFound() {
+    void testDeleteCouponNotFound() {
         long couponId = 5L;
         when(couponRepository.findById(couponId)).thenReturn(Optional.empty());
 
